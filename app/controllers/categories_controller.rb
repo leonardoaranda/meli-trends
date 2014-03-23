2fda5546a354e6c2955779b35404820c61393721
Leonardo Aranda <leonardo.aranda@gmail.com>
2014-03-23 15:39:43 -0300

    add max depth config

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -69,6 +69,6 @@ class CategoriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def category_params
-      params.require(:category).permit(:category_id, :name, :parent_id, :site_id)
+      params.require(:category).permit(:category_id, :name, :parent_id, :site_id, :level)
     end
 end
--- app/controllers/mercadolibre_controller.rb
+++ app/controllers/mercadolibre_controller.rb
@@ -1,5 +1,9 @@
 class MercadolibreController < ApplicationController
 
+	def initialize()
+		@max_depth = 1
+	end
+
 	def get_category_trends(category_id,site_id)
 		request = Net::HTTP.get_response(URI.parse('https://api.mercadolibre.com/sites/'+site_id+'/trends/search?category='+category_id))
 		if request.code == '200'
@@ -21,46 +25,49 @@ class MercadolibreController < ApplicationController
 		return categories
 	end
 
-	def bulk_trends_processor(site_id)
+	def bulk_trends_processor(site_id,max_depth)
+		@max_depth = max_depth
 		categories = get_site_categories(site_id)
 		categories.each do |category|
+			level = 1
 			category_trends = get_category_trends(category['id'],site_id)
 			puts '#####'+category['name']+'#####'			
-			puts category_trends
-			tree_bound(category['id'],nil,nil)
+			tree_bound(category['id'],nil,nil,level)
 		end
 	end
 
-	def tree_bound(category_id,parent_id,parent_trends)
+	def tree_bound(category_id,parent_id,parent_trends,level)
+		level += 1
 		children_categories = get_children_categories(category_id)
 		children_categories.each do |children|
-			if parent_id != nil and parent_trends == nil
+			if (parent_id != nil and parent_trends == nil) or level > @max_depth
 				puts 'SKIPPING '+children['id']+' '+children['name']
 				next
 			end
 			children_category_trends = get_category_trends(children['id'],'MLA')
 			puts '#####'+children['name']+'#####'
-			puts children_category_trends
-			tree_bound(children['id'],category_id,children_category_trends)
+			tree_bound(children['id'],category_id,children_category_trends,level)
 		end
 	end
 
-	def save_categories(site_id)
+	def save_categories(site_id,max_depth)
+		@max_depth = max_depth
 		categories = get_site_categories(site_id)
 		categories.each do |category|
-			level = '-	'
-			puts level+'root '+category['name']+'-'+category['id']
+			level = 1
+			puts level.to_s+'root '+category['name']+'-'+category['id']
 			process_children_categories(category,level)
 		end
 	end
 
 	def process_children_categories(parent,level)
-		level = level + '	-'
-		children_categories = get_children_categories(parent['id'])
-		children_categories.each do |children|
-			puts level+'children '+children['name']+'-'+children['id']
-			process_children_categories(children,level)
+		level += 1
+		if level <= @max_depth
+			children_categories = get_children_categories(parent['id'])
+			children_categories.each do |children|
+				puts level.to_s+'children '+children['name']+'-'+children['id']
+				process_children_categories(children,level)
+			end	
 		end
 	end
-
 end
